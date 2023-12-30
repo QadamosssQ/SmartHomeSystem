ebe237736a3b520b0d1cd44af8c10642be6ed6e8
MSAdamoss <95488005+QadamosssQ@users.noreply.github.com>
2023-12-30 11:51:46 +0100

    Add files

---
+++ src/Clients/DoorLock/request.cpp
@@ -0,0 +1,28 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+
+String make_request(String url){
+
+  WiFiClient wifi;
+  HTTPClient http;
+
+  http.begin(wifi, URL);
+
+  int httpResponseCode = http.GET();
+
+  if (httpResponseCode == 200) {
+    String response = http.getString();
+
+    http.end();
+    return response;
+
+  }else{
+    Serial.print("HTTP Error: ");
+    Serial.println(httpResponseCode);
+    http.end();
+    return "error";
+  }
+
+}
+
