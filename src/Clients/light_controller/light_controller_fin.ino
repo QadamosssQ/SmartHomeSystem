49400ef8eae75d30d6aa2a58c4d9adc170231e3c
QadamosssQ <altadamoss@gmail.com>
2024-05-26 13:53:19 +0200

    light_controller_fin.ino not working due HTTP error -1???

--- src/Clients/light_controller/light_controller_fin.ino
+++ src/Clients/light_controller/light_controller_fin.ino
@@ -1,8 +1,14 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include "request.h"
+#include <ArduinoJson.h>
 
-const char* ssid = "ESP8266-Access-Point";
-const char* password = "123456789";
+#define light D4
+
+const char* APssid = "ESP8266-Access-Point";
+const char* APpassword = "123456789";
+
+const char* apiURL = "http://192.168.0.244:5000/api/GetLightController";
 
 ESP8266WebServer server(80);
 IPAddress apIP(192, 168, 1, 1);
@@ -137,9 +143,13 @@ void handleConnect() {
   server.send(400, "text/plain", "Error: Missing ssid or password parameters");
 }
 
+
 void setup() {
   Serial.begin(115200);
-  WiFi.softAP(ssid, password);
+
+  pinMode(light, OUTPUT);
+
+  WiFi.softAP(APssid, APpassword);
   WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
   IPAddress IP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
@@ -154,7 +164,6 @@ void setup() {
   });
 
   server.on("/connect", HTTP_POST, handleConnect);
-
   server.begin();
 }
 
@@ -172,14 +181,48 @@ void loop() {
     WiFi.softAPdisconnect();
     WiFi.softAPdisconnect(false);
 
-    if (WiFiPing()) {
-    //     here code
-
-
-
+    if(WiFiPing()) {
+      Serial.println("Internet connectivity is available.");
     } else {
       Serial.println("No Internet connectivity.");
     }
+
+    DynamicJsonDocument jsonDoc(1024);
+    jsonDoc["user_secret"] = "sAHZZN8xXS1HMk9dSHU0NHaYJPE_oUcZrESRnH26rhg";
+    jsonDoc["device_secret"] = "Aki3hjZ3T6Vekm-Hr1zge_jYT-Tw0Zl7VsWGiFaUBMM";
+
+    JsonObject jsonObject = jsonDoc.as<JsonObject>();
+
+    String response = make_request(apiURL, jsonObject);
+
+    DynamicJsonDocument jsonDoc2(1024);
+    DeserializationError error = deserializeJson(jsonDoc2, response);
+
+    if (error) {
+      Serial.print("Deserialization failed: ");
+      Serial.println(error.c_str());
+      return;
+    }
+
+    if (jsonDoc2.containsKey("state")) {
+      String stateValue = jsonDoc2["state"];
+      Serial.print("State: ");
+      Serial.println(stateValue);
+
+      if (stateValue == "on") {
+        digitalWrite(light, HIGH);
+      } else if (stateValue == "off") {
+        digitalWrite(light, LOW);
+      } else {
+        Serial.println("JSON structure is not as expected.");
+      }
+
+    } else {
+      Serial.println("error");
+    }
+
+    delay(500);
+
   } else {
     Serial.println("No Internet connectivity.");
   }
